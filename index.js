22196561f3ba108f42da72093bc764e87fc0806c
Simon Burdall <Simon@burdall.com>
2018-06-29 07:56:41 +0100

    Added 'Contact Form' and applied fixes.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function navigationSticky() {
+window.onscroll = function() {addStick()};
 
 // Get the header
-var header = document.getElementById("site-navigation");
+var header = document.getElementById("site-navi-ui");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  if (window.pageYOffset > sticky) {
+function addStick() {
+ if (window.pageYOffset > sticky) {
     header.classList.add("site-navigation-sticky");
   } else {
     header.classList.remove("site-navigation-sticky");
